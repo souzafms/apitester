41482cbc48dc7dd778fa897deb936076ae5687d1
Felipe M de Souza <30316469+souzafms@users.noreply.github.com>
2018-11-12 21:25:30 -0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+
+IP_servidor = input("Digite o IP do servidor: ")
+usuario_servidor = input("Digite o usuario do servidor: ")
+senha_servidor = input("Digite a senha do servidor: ")
