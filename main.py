088e8c6a3d5f864f6dde442fcd338b3d35448858
Felipe M de Souza <30316469+souzafms@users.noreply.github.com>
2018-11-13 15:15:07 -0200

    criada autenticacao

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 import ipaddress
+import comandos_api
 
 ##### O proximo bloco solicita ao usuário o IP do servidor e verifica se é um IP válido
 IP = 0
 while IP == 0:
-	IP_servidor = input("Digite o IP do servidor (ex 192.168.0.1): ")
+	IP_servidor = "192.168.5.5" #input("Digite o IP do servidor (ex 192.168.0.1): ")
 	IP = 1
 	try:
 		valida_ip = ipaddress.ip_address(IP_servidor)
@@ -13,18 +14,9 @@ while IP == 0:
 		IP = 0
 
 ##### Solicitamos usuário e senha sem validação
-usuario_servidor = input("Digite o usuario do servidor: ")
-senha_servidor = input("Digite a senha do servidor: ")
+usuario_servidor = "admin" #input("Digite o usuario do servidor: ")
+senha_servidor = "mdvr%6" #input("Digite a senha do servidor: ")
 
-##### O próximo bloco trata a autenticação para obter a chave de autenticação do servidor
-##### http:///api/v1/basic/key?username=admin&password=admin
-#			|data: data result
-#Resposta:	|--- key: verification key
-#			|errorcode: error code
+autentica_servidor = comandos_api.autentica(IP_servidor, usuario_servidor, senha_servidor)
 
-def autentica (IP_servidor, usuario_servidor, senha_servidor):
-	URL_autenticacao = "http://" + str(IP_servidor) + "/api/v1/basic/key?username=" + str(usuario_servidor) + "&password=" + str(senha_servidor)
-	autenticacao = requests.get(url = URL_autenticacao)
-	return autenticacao
-
-print(URL_autenticacao)
+print(autentica_servidor)
