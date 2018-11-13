e24297f726bc1ae479d08089999d724f58d00830
Felipe M de Souza <30316469+souzafms@users.noreply.github.com>
2018-11-13 12:39:57 -0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #import requests
-import socket
+import ipaddress
 
+##### O proximo bloco solicita ao usuário o IP do servidor e verifica se é um IP válido
+##### I
 IP = 0
 while IP == 0:
-	IP_servidor = input("Digite o IP do servidor: ")
+	IP_servidor = input("Digite o IP do servidor (ex 192.168.0.1): ")
+	IP = 1
 	try:
-		socket.inet_aton(IP_servidor)
-	except socket.error:
+		valida_ip = ipaddress.ip_address(IP_servidor)
+	except:
 		print("IP Inválido")
-		
+		IP = 0
 usuario_servidor = input("Digite o usuario do servidor: ")
 senha_servidor = input("Digite a senha do servidor: ")
 
